70be5fd74a62b0c8bff64bb06139615052226188
SilverEmber <strikerdanger0099@gmail.com>
2023-04-27 13:28:34 +1000

    Added more pages, css, banner testing. Needs vertical text centering. Can't seem to link the css? Added favicon, idk how to serve the favicon.

--- src/handlers.rs
+++ src/handlers.rs
@@ -16,6 +16,18 @@ pub async fn index(req: HttpRequest) -> actix_web::Result<impl Responder> {
 
     Ok(HttpResponse::Ok().body(body))
 }
+pub async fn page_tags(req: HttpRequest) -> actix_web::Result<impl Responder> {
+    let body = templates::Tags {
+		}.render_once()
+        .map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().body(body))
+}
+pub async fn page_bannertest(req: HttpRequest) -> actix_web::Result<impl Responder> {
+    let body = templates::BannerTest{}
+        .render_once()
+        .map_err(error::ErrorInternalServerError)?;
+    Ok(HttpResponse::Ok().body(body))
+}
 
 pub async fn jsontest(
     data: web::Data<info_structs::AppState>,
--- src/handlers/templates.rs
+++ src/handlers/templates.rs
@@ -4,3 +4,13 @@ use sailfish::TemplateOnce;
 #[template(path = "home.stpl")]
 pub struct Home {
 }
+
+#[derive(TemplateOnce)]
+#[template(path = "tags.stpl")]
+pub struct Tags {
+}
+
+#[derive(TemplateOnce)]
+#[template(path = "banner_test.stpl")]
+pub struct BannerTest {
+}
